4140298da7cab2b9f7d7ef2a220ee83063c4791d
Lucas <lucas.santos@mundowap.com.br>
2022-11-05 12:06:50 -0300

    feat: done forture cookie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,98 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import cookie1 from './assets/biscoito.png';
+import cookie2 from './assets/biscoitoAberto.png';
+import quotes from './data/quotes';
 
 export default function App() {
+  const [message, setMessage] = useState(null);
+
+  function showMessage() {
+    let randomNumber = Math.floor(Math.random() * (quotes.length - 0 + 1) + 0);
+    setMessage(quotes[randomNumber]);
+  }
+
+  function resetCookie() {
+    setMessage(null);
+  }
   return (
-    <View>
-      <Text> Biscoito da sorte </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}> Biscoito da sorte </Text>
+      <Image
+        source={message !== null ? cookie2 : cookie1}
+        style={styles.cookieImage}
+      />
+      {message !== null && (
+        <View style={styles.messageContainer}>
+          <Text style={styles.message}> "{message}" </Text>
+        </View>
+      )}
+      <View style={styles.containerButton}>
+        <TouchableOpacity style={styles.btn1} onPress={showMessage}>
+          <Text style={styles.bnt1Text}> Quebrar Biscoito </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn2} onPress={resetCookie}>
+          <Text> Reiniciar Biscoito </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 20,
+    color: '#c8c8c8',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+
+  messageContainer: {},
+  message: {
+    color: '#FD841F',
+    fontSize: 13,
+    textAlign: 'center',
+  },
+  cookieImage: {
+    width: 200,
+    height: 200,
+  },
+
+  containerButton: {
+    marginTop: 16,
+  },
+
+  btn1: {
+    width: 320,
+    height: 42,
+    borderWidth: 1,
+    borderColor: '#FD841F',
+    borderStyle: 'solid',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  bnt1Text: {
+    color: '#FD841F',
+    fontWeight: 'bold',
+  },
+
+  btn2: {
+    marginTop: 8,
+    width: 320,
+    height: 42,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderStyle: 'solid',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/data/quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+  'Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.',
+  'Imagine uma nova história para sua vida e acredite nela.',
+  'A amizade desenvolve a felicidade e reduz o sofrimento, duplicando a nossa alegria e dividindo a nossa dor.',
+  'Nunca deixe ninguém te dizer que não pode fazer alguma coisa. Se você tem um sonho tem que correr atrás dele. As pessoas não conseguem vencer e dizem que você também não vai vencer. Se você quer uma coisa corre atrás.',
+  'Ser feliz sem motivo é a mais autêntica forma de felicidade.',
+  'Não existe um caminho para a felicidade. A felicidade é o caminho.',
+  'Não espere por uma crise para descobrir o que é importante em sua vida',
+  'Saber encontrar a alegria na alegria dos outros é o segredo da felicidade.',
+  'A alegria de fazer o bem é a única felicidade verdadeira.',
+  'Acredite em si próprio e chegará um dia em que os outros não terão outra escolha senão acreditar com você.',
+  'Não permito que nenhuma reflexão filosófica me tire a alegria das coisas simples da vida.',
+  'É bem difícil descobrir o que gera a felicidade; pobreza e riqueza falharam nisso.',
+  'Pessimismo leva à fraqueza, otimismo ao poder.',
+  'O otimismo é a fé em ação. Nada se pode levar a efeito sem otimismo.',
+  'O pessimismo, depois de você se acostumar a ele, é tão agradável quanto o otimismo.',
+];
+
+export default quotes;
